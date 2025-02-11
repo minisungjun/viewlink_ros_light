cdcb0fec4fa37ac822508723b11a1935d90a1456
sungjun <minisungjun@naver.com>
2025-02-12 00:56:39 +0900

    Tunned for robust operation

--- src/tcp_ver.cpp
+++ src/tcp_ver.cpp
@@ -1,5 +1,5 @@
 #include <ros/ros.h>
-#include <std_msgs/Float32MultiArray.h>
+#include <geometry_msgs/Vector3.h>
 #include "ViewLink.h"
 #include "kari_dronecop_rd_payload_mgmt/payload_mc_gimbal_ctrl_cmd.h"
 
@@ -8,11 +8,11 @@ public:
     Gimbal(ros::NodeHandle& nh, ros::NodeHandle& nhp) {
         // ROS Publisher & Subscriber 설정
         cmd_sub = nh.subscribe("gimbal_cmd_deg", 1, &Gimbal::gimbalCmdCallback, this);
-        pose_pub = nh.advertise<std_msgs::Float32MultiArray>("gimbal_pose_deg", 10);
+        pose_pub = nh.advertise<geometry_msgs::Vector3>("gimbal_pose_deg", 10);
         gcs_cmd_sub = nh.subscribe("gcs_cmd", 1, &Gimbal::gcsCmdCallback, this);
 
-        rate_hz = nhp.param<double>("rate", 30);
-        ros::Rate rate(rate_hz);  // 30Hz
+        rate_ms = nhp.param<int>("rate_ms", 200);
+        rate_ms = std::min(std::max(rate_ms, 100), 5000);
         cmd_multiple = nhp.param<double>("cmd_multiple", 10000);
 
         // ViewLink SDK 초기화
@@ -23,20 +23,25 @@ public:
 
         // Register device status callback
         VLK_RegisterDevStatusCB(Gimbal::DeviceStatusCallback, this);
-        VLK_SetKeepAliveInterval(100);
+        VLK_SetKeepAliveInterval(rate_ms);
 
         // TCP 연결 설정
         VLK_CONN_PARAM connParam;
         connParam.emType = VLK_CONN_TYPE_TCP;
-        strcpy(connParam.ConnParam.IPAddr.szIPV4, "10.10.10.102");  // 짐벌의 IP 주소
+        strcpy(connParam.ConnParam.IPAddr.szIPV4, nhp.param<std::string>("gimbal_ip", "10.10.10.102").c_str());  // 짐벌의 IP 주소
         connParam.ConnParam.IPAddr.iPort = 2000;  // 짐벌의 TCP 포트
 
         // 연결 시도
-        if (VLK_Connect(&connParam, nullptr, nullptr) != VLK_ERROR_NO_ERROR) {
-            ROS_ERROR("Failed to connect to gimbal via TCP");
+        VLK_Connect(&connParam, nullptr, nullptr);
+        // if ( != VLK_ERROR_NO_ERROR) {
+        //     ROS_ERROR("Failed to connect to gimbal via TCP");
+        //     ros::shutdown();
+        // }
+        // @TODO Fix detecting ill connection
+        if (!VLK_IsTCPConnected()) {
+            ROS_WARN_THROTTLE(1, "Gimbal not connected");
             ros::shutdown();
         }
-
         ROS_INFO("Gimbal connected successfully");
     }
 
@@ -63,19 +68,15 @@ public:
         getGimbalPose();
     }
 
-    void gimbalCmdCallback(const std_msgs::Float32MultiArray::ConstPtr& msg) {
+    void gimbalCmdCallback(const geometry_msgs::Vector3::ConstPtr& msg) {
         std::lock_guard<std::mutex> lock(telemetry_mutex);
         // std::cout << "gimbalCmdCB" <<std::endl;
-        if (msg->data.size() < 3) {
-            ROS_WARN("Received invalid gimbal command");
-            return;
-        }
 
-        double roll = msg->data[0];   // 롤 값
-        double pitch = msg->data[1];  // 피치 값
-        double yaw = msg->data[2];    // 요 값
+        double roll = msg->x;   // 롤 값
+        double pitch = msg->y;  // 피치 값
+        double yaw = msg->z;    // 요 값
 
-        ROS_INFO("Setting Gimbal Pose - Roll: %.2f, Pitch: %.2f, Yaw: %.2f", roll, pitch, yaw);
+        ROS_INFO("Setting Gimbal Pose - Pitch: %.2f, Yaw: %.2f", pitch, yaw);
 
         // ViewLink SDK를 사용하여 짐벌 움직이기
         VLK_TurnTo(yaw, pitch);  // 짐벌은 Roll을 직접 지원하지 않으므로 Pitch, Yaw만 사용
@@ -129,16 +130,15 @@ public:
         }
 
         // Publish pose data
-        std_msgs::Float32MultiArray pose_msg;
-        pose_msg.data.resize(3);
-        pose_msg.data[0] = 0.0;                // Roll (짐벌에서 지원 X)
-        pose_msg.data[1] = telemetry.dPitch;   // Pitch
-        pose_msg.data[2] = telemetry.dYaw;     // Yaw
+        geometry_msgs::Vector3 pose_msg;
+        pose_msg.x = 0.0;                // Roll (짐벌에서 지원 X)
+        pose_msg.y = telemetry.dPitch;   // Pitch
+        pose_msg.z = telemetry.dYaw;     // Yaw
 
         pose_pub.publish(pose_msg);
     }
 
-    double rate_hz;
+    int rate_ms;
     double cmd_multiple;
 
 private:
@@ -156,13 +156,8 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh, nhp("~");
 
     Gimbal gimbal(nh, nhp);
-    ros::Rate rate(gimbal.rate_hz);
+    ros::Rate rate(gimbal.rate_ms);
 
-    // while (ros::ok()) {
-    //     // gimbal.getGimbalPose();  // Now we can safely call this
-    //     ros::spinOnce();
-    //     rate.sleep();
-    // }
     ros::spin();
 
     return 0;
